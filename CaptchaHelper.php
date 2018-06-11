70be456a3063fa29ae2e7d025101d13c9c89c4e4
Vlad Deykhin <deykhin@gmail.com>
2018-06-11 12:57:15 +0300

    Added extension code

---
+++ CaptchaHelper.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace venomousboy\yii2-rest-api-captcha;
+
+use yii\captcha\CaptchaAction;
+use yii\base\Exception;
+use Yii;
+
+class CaptchaHelper extends CaptchaAction
+{
+    private $code;
+
+    /**
+     * CaptchaHelper constructor.
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function generateImage(): string
+    {
+        $base64 = "data:image/png;base64," . base64_encode($this->renderImage($this->generateCode()));
+        Yii::$app->session->set($this->generateSessionKey($this->generateCode()), $this->generateCode());
+        return $base64;
+    }
+
+    /**
+     * @return string
+     */
+    public function generateCode(): string
+    {
+        if ($this->code) {
+            return $this->code;
+        }
+
+        return $this->code = $this->generateVerifyCode();
+    }
+
+    /**
+     * @param string $code
+     * @return bool
+     * @throws Exception
+     */
+    public function verify(string $code): bool
+    {
+        if (Yii::$app->session->get($this->generateSessionKey($code)) === $code) {
+            return true;
+        }
+
+        throw new Exception("Code is not valid \"{$code}\"");
+    }
+
+    /**
+     * @return string
+     */
+    private function generateSessionKey(string $code): string
+    {
+        return base64_encode(Yii::$app->request->getRemoteIP() . Yii::$app->request->getUserAgent() . $code);
+    }
+}
